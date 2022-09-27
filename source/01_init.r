c5d72672c7d31147adbb4e7b1dab6067fe23a29e
Eszter Ari <arieszter@gmail.com>
2022-09-27 21:55:37 +0200

    init

---
+++ source/01_init.r
@@ -0,0 +1,19 @@
+# library(devtools)
+# install_github("https://github.com/koralgooll/MulEA.git")
+library(MulEA)
+#xxxxxxxxxxxxxxxxxxxxx
+
+# import example gene set
+# import other gene sets from a GMT file using read_gmt()
+data(geneSet)
+# TODO: rename: geneSet -> example_gmt
+
+# import "WHAT ARE WE IMPORTING HERE?"
+data(selectDf)
+# TODO: rename: selectDf -> example_element_names
+
+# import "WHAT ARE WE IMPORTING HERE?"
+data(poolDf)
+# TODO rename: poolDf -> example_background_element_names
+
+
